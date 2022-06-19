370ba1ec345fe808538178254ae0748a3609ec23
Omar El Garwany <oe2@sanger.ac.uk>
2022-06-19 18:19:10 +0100

    add paths/configs files

--- paths.sh
+++ paths.sh
@@ -7,3 +7,6 @@ BDIR=/lustre/scratch123/hgi/mdt2/projects/ibdgwas_bioresource/qz2/post_imputatio
 IDPSNPS_DIR="$IBDBR_BASEDIR"output/idpsnps/
 IBDBR_SNAKEMAKE_WORKDIR="$IBDBR_BASEDIR"snakemake_workdirs/
 exclude_regions_f=/lustre/scratch123/hgi/mdt2/projects/ibdgwas_bioresource/qz2/ref/high-LD-regions-hg38-GRCh38.txt
+
+GRM_GWAS_DIR="$IBDBR_BASEDIR"output/grm/gwas/
+GCTA_BIN=/software/team152/oe2/anaconda3/envs/ibdbr/bin/gcta64
